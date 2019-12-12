62689729408f3800a429654e85bf489ee88a3f01
yani-lab <2536496956@qq.com>
2019-12-12 16:11:41 +0800

    第四次提交，所有controller的类全部建完

---
+++ src/main/java/com/zking/ssm/controller/info/TProducttableController.java
@@ -0,0 +1,8 @@
+package com.zking.ssm.controller.info;
+/*
+@author yani
+@create 2019-12-1216:02
+*/
+
+public class TProducttableController {
+}
---
+++ src/main/java/com/zking/ssm/controller/info/TUserinfoController.java
@@ -0,0 +1,8 @@
+package com.zking.ssm.controller.info;
+/*
+@author yani
+@create 2019-12-1216:04
+*/
+
+public class TUserinfoController {
+}
