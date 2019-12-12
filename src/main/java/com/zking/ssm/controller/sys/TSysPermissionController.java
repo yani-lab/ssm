ebfa154333aa618504a80413100a9c398aeb6976
yani-lab <2536496956@qq.com>
2019-12-12 16:14:11 +0800

    第四次提交，所有controller的类全部建完

---
+++ src/main/java/com/zking/ssm/controller/sys/TSysPermissionController.java
@@ -0,0 +1,8 @@
+package com.zking.ssm.controller.sys;
+/*
+@author yani
+@create 2019-12-1215:45
+*/
+
+public class TSysPermissionController {
+}
---
+++ src/main/java/com/zking/ssm/controller/sys/TSysRoleController.java
@@ -0,0 +1,8 @@
+package com.zking.ssm.controller.sys;
+/*
+@author yani
+@create 2019-12-1215:46
+*/
+
+public class TSysRoleController {
+}
