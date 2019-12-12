6f74bc49411ac93ee83fb027b3b4bb30d8a80033
yani-lab <2536496956@qq.com>
2019-12-12 16:14:32 +0800

    第四次提交，所有controller的类全部建完

---
+++ src/main/java/com/zking/ssm/controller/vip/TIntegralController.java
@@ -0,0 +1,8 @@
+package com.zking.ssm.controller.vip;
+/*
+@author yani
+@create 2019-12-1215:41
+*/
+
+public class TIntegralController {
+}
---
+++ src/main/java/com/zking/ssm/controller/vip/TIntegralListController.java
@@ -0,0 +1,8 @@
+package com.zking.ssm.controller.vip;
+/*
+@author yani
+@create 2019-12-1215:42
+*/
+
+public class TIntegralListController {
+}
