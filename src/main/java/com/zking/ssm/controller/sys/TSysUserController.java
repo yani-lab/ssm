9c58f3908cf09ccdae35b124eca896a911bcb8e3
luoxiang-1 <2636103307@qq.com>
2019-12-14 17:08:22 +0800

    登录

--- src/main/java/com/zking/ssm/controller/sys/TSysUserController.java
+++ src/main/java/com/zking/ssm/controller/sys/TSysUserController.java
@@ -3,6 +3,9 @@ package com.zking.ssm.controller.sys;
 import com.zking.ssm.model.sys.TSysUser;
 import com.zking.ssm.service.sys.ISysUserService;
 import com.zking.ssm.util.PasswordHelper;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,12 +22,24 @@ public class TSysUserController {
     private ISysUserService sysUserService;
     @RequestMapping("/register")
     public void regiestUser(TSysUser sysUser){
-        PasswordHelper.createSalt();
-        PasswordHelper.createCredentials(sysUser.getUserPwd(),sysUser.getUserPwd());
-
-
+        String salt=PasswordHelper.createSalt();
+       String userPwd= PasswordHelper.createCredentials(sysUser.getUserPwd(),salt);
+        sysUser.setUserPwd(userPwd);
+        sysUser.setSalt(salt);
         sysUserService.RegisterUser(sysUser);
 
+    }
 
+    public void login(TSysUser user){
+        String userName=user.getUserName();
+        String userPwd=user.getUserPwd();
+
+        String Pwd= PasswordHelper.createCredentials(userPwd,user.getSalt());
+        boolean b = PasswordHelper.checkCredentials(userPwd,user.getSalt(),Pwd);
+
+        sysUserService.login(userName);
+        Subject subject= SecurityUtils.getSubject();
+        UsernamePasswordToken token=new UsernamePasswordToken(user.getUserName(),user.getUserPwd());
+        subject.login(token);
     }
 }
