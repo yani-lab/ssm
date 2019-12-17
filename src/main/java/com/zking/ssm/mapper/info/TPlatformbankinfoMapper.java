77f4f0c78f87282d98663166960a66093cd0bda2
luoxiang-1 <2636103307@qq.com>
2019-12-17 10:03:01 +0800

    银行卡新增和查看Mapper模块

--- src/main/java/com/zking/ssm/mapper/info/TPlatformbankinfoMapper.java
+++ src/main/java/com/zking/ssm/mapper/info/TPlatformbankinfoMapper.java
@@ -1,9 +1,20 @@
 package com.zking.ssm.mapper.info;
 
+import com.zking.ssm.model.info.TPlatformbankinfo;
+
+import java.util.List;
+
 /**
  * @author luo
  * @company zking
  * @creat 2019-12-1216:24
  */
 public interface TPlatformbankinfoMapper {
+    /**
+     * 新增银行卡
+     * @param platformbankinfo
+     * @return
+     */
+    int addBank(TPlatformbankinfo platformbankinfo);
+    List<TPlatformbankinfo> selectBank(TPlatformbankinfo platformbankinfo);
 }
