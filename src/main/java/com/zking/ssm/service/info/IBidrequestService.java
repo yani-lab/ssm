f36456c9b7710f45bb5611cf54cd900ee8aaccc9
xiesuqin-44 <741498238@qq.com>
2019-12-24 14:09:07 +0800

    借款Service

--- src/main/java/com/zking/ssm/service/info/IBidrequestService.java
+++ src/main/java/com/zking/ssm/service/info/IBidrequestService.java
@@ -4,5 +4,31 @@ package com.zking.ssm.service.info;
 @create 2019-12-1216:20
 */
 
+import com.zking.ssm.model.info.TBidrequest;
+import com.zking.ssm.util.PageBean;
+
+import java.util.List;
+
 public interface IBidrequestService {
+
+    /**
+     * 查询借款
+     * @param tBidrequest
+     * @return
+     */
+    List<TBidrequest> listTBidrequest(TBidrequest tBidrequest);
+
+
+    List<TBidrequest> listTBidrequestPage(TBidrequest tBidrequest, PageBean pageBean);
+
+
+    /**
+     * 获取所有的产品类型
+     * @param bidrequesttype
+     * @return
+     */
+    List<TBidrequest> getbidRequestTypeAll(TBidrequest bidrequesttype);
+
+
+
 }
